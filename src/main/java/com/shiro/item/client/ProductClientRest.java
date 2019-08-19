f1ebbaac81935fa2f8ececde418fc9495f9253cf
Albano Yanes <ajyanreyu@gmail.com>
2019-08-19 20:16:05 +0100

    Define service and controller feign

---
+++ src/main/java/com/shiro/item/client/ProductClientRest.java
@@ -0,0 +1,23 @@
+package com.shiro.item.client;
+
+import com.shiro.item.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "product-service", url = "127.0.0.1:8000")
+public interface ProductClientRest {
+    /**
+     * list all products
+     * GetMapping indicates the route of the endpoint to be consumed
+     * @return List<Product>
+     */
+    @GetMapping("/list")
+    public List<Product> list();
+
+    @GetMapping("/details")
+    public Product productById(@RequestParam(name = "id") Long id);
+
+}
---
+++ src/main/java/com/shiro/item/service/ItemServiceFeign.java
@@ -0,0 +1,27 @@
+package com.shiro.item.service;
+
+import com.shiro.item.client.ProductClientRest;
+import com.shiro.item.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+public class ItemServiceFeign  implements ItemServiceInterface{
+
+    @Autowired
+    private ProductClientRest productClientRest;
+    @Override
+    public List<Item> findAll() {
+        return productClientRest.list().stream().map(product -> new Item(product, 1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, Integer number) {
+        return new Item(productClientRest.productById(id), number);
+    }
+}
