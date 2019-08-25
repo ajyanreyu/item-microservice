3e879eb7538d43e0ded94f7aefa3a1e859eb9304
Albano Yanes <ajyanreyu@gmail.com>
2019-08-25 16:10:27 +0100

    Added conect to configuration server

--- src/main/java/com/shiro/item/controller/ItemController.java
+++ src/main/java/com/shiro/item/controller/ItemController.java
@@ -2,19 +2,32 @@ package com.shiro.item.controller;
 
 import com.shiro.item.model.Item;
 import com.shiro.item.service.ItemServiceInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ItemController {
+
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
     @Autowired
     @Qualifier("serviceFeign")
     private ItemServiceInterface itemService;
+
+    @Value("${config.text}")
+    private String text;
 
     @GetMapping("/list")
     public List<Item> list() {
@@ -24,6 +37,15 @@ public class ItemController {
     @GetMapping("/details")
     public Item details(@RequestParam(name = "id") Long id, @RequestParam(name = "number") Integer number) {
         return itemService.findById(id, number);
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<?> getConfig(@Value("${server.port}") String port) {
+        log.info(text);
+        Map<String, String> json = new HashMap<>();
+        json.put("text", text);
+        json.put("port", port);
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 
 }
